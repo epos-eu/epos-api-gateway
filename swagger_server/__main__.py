f6130e398388bc90cc8013de3b5b5d25e3cc21c7
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2024-08-20 13:30:23 +0200

    update

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -354,10 +354,8 @@ def main():
         script_name=path
     )
     flask_app.wsgi_app = proxied
-    #app.run(port=5000)
+    app.run(port=5000)
 
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
 
 if __name__ == '__main__':
     main()
